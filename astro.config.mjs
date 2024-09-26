ee081977478f09e951c313c59d90f8b2396a52b3
Brad <bbodine1@gmail.com>
2024-09-25 22:02:41 -0500

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
