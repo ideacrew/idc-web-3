d02ee0fddaa681524ed8f46b3e8fe47c57717cde
Brad Bodine <bbodine1@gmail.com>
2024-11-12 17:42:18 -0600

    add sanity to app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,30 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import sanityIntegration from '@sanity/astro'
+import react from '@astrojs/react'
+import { loadEnv } from 'vite'
+import netlify from '@astrojs/netlify'
+
+const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(
+	process.env.NODE_ENV,
+	process.cwd(),
+	''
+)
+import react from '@astrojs/react'
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
-});
+	output: 'hybrid',
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sanityIntegration({
+			projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
+			dataset: PUBLIC_SANITY_STUDIO_DATASET,
+			useCdn: true,
+			studioBasePath: '/admin',
+		}),
+		react(),
+	],
+	adapter: netlify(),
+})
