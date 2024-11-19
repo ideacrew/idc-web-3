e6c7e0d2ac71397c653a5b6228c2ef6a05801849
Brad Bodine <bbodine1@gmail.com>
2024-11-19 10:35:11 -0600

    remove sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,14 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-import sanityIntegration from '@sanity/astro'
 import react from '@astrojs/react'
 import { loadEnv } from 'vite'
 import netlify from '@astrojs/netlify'
-
-const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv(
-	process.env.NODE_ENV,
-	process.cwd(),
-	''
-)
 
 export default defineConfig({
 	output: 'hybrid',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
-		}),
-		sanityIntegration({
-			projectId: PUBLIC_SANITY_STUDIO_PROJECT_ID,
-			dataset: PUBLIC_SANITY_STUDIO_DATASET,
-			useCdn: true,
-			studioBasePath: '/admin',
 		}),
 		react(),
 	],
