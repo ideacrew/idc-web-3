2ce6c0d2a0f199f746169a93467d6d1a9370d3fb
Brad Bodine <bbodine1@gmail.com>
2024-12-03 09:28:06 -0600

    Refactor index page to use YAML for services and clients; update Astro config to include YAML plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import { loadEnv } from 'vite'
 import netlify from '@astrojs/netlify'
+import yaml from '@rollup/plugin-yaml'
 
 export default defineConfig({
 	output: 'hybrid',
@@ -13,4 +13,7 @@ export default defineConfig({
 		react(),
 	],
 	adapter: netlify(),
+	vite: {
+		plugins: [yaml()],
+	},
 })
